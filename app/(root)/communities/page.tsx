ca467572f8cf71e1fc6b853aeede2bbec9ceb99d
Shubhodeep <banerjeeshubho98@gmail.com>
2023-09-15 14:24:34 +0530

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = async () => {
+    return (
+        <section>
+            <h1 className='head-text mb-5'>
+                Community.
+            </h1>
+        </section>
+    )
+}
+
+export default Page;
---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import {sidebarLinks} from "@/constants";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import {usePathname, useRouter} from 'next/navigation';
+
+function LeftSidebar() {
+    const router = useRouter();
+    const pathname = usePathname();
+    const { userId } = useAuth();
+    return (
+    <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-4 px-4">
+            {sidebarLinks.map((link)=> {
+                const isActive = (pathname.includes(link.route) && link.route.length > 1) ||
+                pathname === link.route;
+                if(link.route === '/profile') link.route = `${link.route}/$ 
+                {userId}`
+
+                return(
+                    <div>
+                        <Link 
+                        href={link.route}
+                        key={link.label}
+                        className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+                        >
+                        <Image 
+                        src={link.imgURL}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                        />
+                        <p className="text-light-1 max-lg:hidden">
+                        {link.label}
+                        </p>
+                        </Link>
+                    </div>
+                )}
+            )} 
+        </div>
+        <div className="mt-10 px-6">
+                <SignedIn>
+                    <SignOutButton signOutCallback={()=> router.push('/sign-in')}>
+                        <div className="flex cursor-pointer gap-4 pt-4">
+                            <Image
+                                src="/assets/logout.svg"
+                                alt="logout"
+                                width={24}
+                                height={24}
+                            />
+                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
+
+        </div>
+    </section>
+    
+    )
+}
+
+export default LeftSidebar;
